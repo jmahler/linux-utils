bb227c451bd05951b15c471c1041caf58c0e1221
Jeremiah Mahler <jmmahler@gmail.com>
2014-04-07 00:01:34 -0700

    signal option to kill
    The kill program will now accept a signal option.
    
        $ kill -s 15 <pid>

--- coreutils/kill.c
+++ coreutils/kill.c
@@ -1,26 +1,44 @@
 
 #include <sys/types.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 
-void usage(char *proc) {
-	printf("usage: %s [options] <pid>\n", proc);
+void usage(char *proc)
+{
+	printf("usage: %s [-s <signal>] <pid>\n", proc);
 }
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
 	pid_t pid;
 	int sig;
 	int res;
-
-	if (argc < 2) {
-		usage(argv[0]);
-		return 1;
-	}
-	pid = atoi(argv[1]);
+	int opt;
+	extern int optind;
+	extern char *optarg;
 
 	sig = SIGTERM;
+	while ((opt = getopt(argc, argv, "s:")) != -1) {
+		switch (opt) {
+		case 's':
+			sig = atoi(optarg);
+			break;
+		default:
+			usage(argv[0]);
+			return 1;
+		}
+	}
+	/* PID is the first non-option argument */
+	if (optind < argc) {
+		pid = atoi(argv[optind]);
+	} else {
+		usage(argv[0]);
+		fprintf(stderr, "  missing PID argument\n");
+		return 1;
+	}
+
 	res = kill(pid, sig);
 	if (-1 == res) {
 		perror("kill");
